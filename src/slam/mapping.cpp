dd6ea51608ba102ad6b71f779673dbe5416b2fc7
Blake Karwoski <bkarw@umich.edu>
2020-03-30 08:31:54 -0500

    tracing out ideas

--- src/slam/mapping.cpp
+++ src/slam/mapping.cpp
@@ -16,4 +16,12 @@ Mapping::Mapping(float maxLaserDistance, int8_t hitOdds, int8_t missOdds)
 void Mapping::updateMap(const lidar_t& scan, const pose_xyt_t& pose, OccupancyGrid& map)
 {
     //////////////// TODO: Implement your occupancy grid algorithm here ///////////////////////
+
+    //loop to iterate through lidar_t scans
+        //check if ranges >= maxLaserDistance. If true, do nothing
+        //Use pose to get starting cell
+        //Use pose + scan range & bearing to get ending cell
+        //For ending cell, add hitOdds to the cell's current CellOdds using setLogOdds
+        //Use Breshenham function to get all empty cells
+            //for each empty cell, increment logodds by missOdds
 }
