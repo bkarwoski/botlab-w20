1cf070f76012dc4de1ac164b8ca263c9d0d09580
Blake Karwoski <bkarw@umich.edu>
2020-04-14 10:46:57 -0500

    printing particle attributes seems to work

--- data/py-decodelog/decode-log.py
+++ data/py-decodelog/decode-log.py
@@ -4,6 +4,7 @@ import lcm
 # put lcm types you need here
 # run make once to generate python lcm types
 from lcmtypes import pose_xyt_t
+from lcmtypes import particles_t
 
 if len(sys.argv) < 2:
     sys.stderr.write("usage: decode-log <logfile>\n")
@@ -12,8 +13,13 @@ if len(sys.argv) < 2:
 log = lcm.EventLog(sys.argv[1], "r")
 
 for event in log:
-    if event.channel == "TRUE_POSE":
-        msg = pose_xyt_t.decode(event.data)
-        print("TruePose:")
-        print("timestamp= %d" % msg.utime)
-        print("pose: (%d, %d, %d)" % (msg.x, msg.y, msg.theta))
+    # if event.channel == "TRUE_POSE":
+    #     msg = pose_xyt_t.decode(event.data)
+    #     print("TruePose:")
+    #     print("timestamp= %d" % msg.utime)
+    #     print("pose: (%d, %d, %d)" % (msg.x, msg.y, msg.theta))
+    if event.channel == "SLAM_PARTICLES":
+        msg = particles_t.decode(event.data)
+        print("Particles: ", end='')
+        print(msg.num_particles, ' ', end='')
+        print(msg.particles[0].pose.x)
